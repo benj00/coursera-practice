4dbf6e7c4c24fd5f1260177563132fbf41a4746f
benj00 <benjamc@yahoo.com>
2025-03-31 09:45:21 -0400

    message

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -34,8 +34,15 @@ function clearCompletedTasks() {
             displayTasks();
         }
 
+function clearAllTasks() {
+            //tasks = tasks.filter(task => !task.completed);
+            tasks = [];
+            displayTasks();
+        }
+
 addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllBtn.addEventListener("click", clearAllTasks);
 displayTasks();
 
     
